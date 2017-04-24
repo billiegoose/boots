9b958c5fc3341e58c9c344aaafd6de2cfe03eb57
Will Hilton <wmhilton@gmail.com>
2017-04-24 19:03:20 -0400

    Added a ./makeusb.sh script that will hopefully stop me from ever overwriting the MBR of the wrong disk.

---
+++ makeusb.sh
@@ -0,0 +1,26 @@
+#! /usr/bin/env bash
+
+make myos.img
+
+SIZE=3789504
+
+# This command is very very careful to only select my USB device by partition size
+DEV=$(cat /proc/partitions | awk '$3 == '${SIZE}' && $5==NIL {print $4}')
+WC=$(echo $DEV | wc -w)
+
+if [[ -z $DEV ]]; then
+  echo "Did not find USB drive"
+elif [[ $WC != 1 ]]; then
+  echo "Found more than one flash drive:"
+  echo "$DEV"
+else
+  echo "Located USB drive: $DEV"
+  read -r -n 1 -p "Blast MBR of /dev/$DEV? [y/N] " result
+  echo ""
+  if [[ "$result" == "y" ]]; then
+    echo "Copying myos.img to /dev/$DEV..."
+    dd if="myos.img" of="/dev/$DEV"
+    sync
+    echo "Complete."
+  fi
+fi
