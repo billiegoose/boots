91736c71993b3faeb7cf6b474022769a765b98b2
Will Hilton <wmhilton@gmail.com>
2017-04-27 15:36:11 -0400

    Fix the annoying off-by-one error in bios.clear_screen

--- bios/screen.asm
+++ bios/screen.asm
@@ -2,9 +2,11 @@
 bios.clear_screen:
 	pusha
 	bios.cursor.moveto 0, 0
-	mov cx, 70
+	bios.cursor.write_char 32
+	; TODO, clear the whole screen lol
+	mov cx, 80
 @@:
-	bios.cursor.moveto 0, cl		; FIXME: doesn't clear 1st column
+	bios.cursor.moveto 0, cl
 	bios.cursor.write_char 32
 	loop @b
 	popa
